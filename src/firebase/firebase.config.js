573e6a4e7db6216173a9d785c1c195d43fb059d3
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-04-04 03:10:10 +0600

    firebase setup including signIn method google & signIn with email & password, added firebase config file etc.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvOltpIaU8J2Yqmz62eQzit_2_R6VM0Io",
+  authDomain: "dragon-news-e5f09.firebaseapp.com",
+  projectId: "dragon-news-e5f09",
+  storageBucket: "dragon-news-e5f09.appspot.com",
+  messagingSenderId: "88816427965",
+  appId: "1:88816427965:web:8d14e94e6a655382e868d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
