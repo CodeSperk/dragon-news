934780e5aab35c3de2c9736b77de1710d5ce6828
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-04-05 01:05:43 +0600

    updated dynamic date from moment js

--- src/components/shared/pageHeader/PageHeader.jsx
+++ src/components/shared/pageHeader/PageHeader.jsx
@@ -1,17 +1,7 @@
+import moment from "moment";
 import logo from "../../../assets/logo.png";
 
 const PageHeader = () => {
-  const fullDate = new Date();
-  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-  const dayName = dayNames[fullDate.getDay()];
-  const monthName = monthNames[fullDate.getMonth()];
-  const date = fullDate.getDate();
-  const year = fullDate.getFullYear();
-  
-  const formattedDate = `${monthName} ${date}, ${year}`;
-
 
   return (
     <div className="py-6 md:py-8 lg:py-10 text-center">
@@ -20,7 +10,13 @@ const PageHeader = () => {
       </div>
       <p>Journalism Without Fear or Favour</p>
       {/* Today */}
-      <p className="md:text-lg mt-3"><span className="font-semibold">{dayName}, </span> {formattedDate}</p>
+
+      <p className="md:text-lg mt-3">
+        <span className="font-semibold">
+          {moment().format("dddd, ")}  
+        </span> 
+          {moment().format("MMMM DD, YYYY")}
+      </p>
     </div>
   );
 };
